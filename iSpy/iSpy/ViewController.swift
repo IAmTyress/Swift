0113f00e5d60bb7c90fa1357d9a857a0f050bfb8
Malysh Tim <malyshtim2000@yandex.ru>
2020-04-24 13:55:09 +0300

    iSpy

---
+++ iSpy/iSpy/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  iSpy
+//
+//  Created by Мас on 31.03.2020.
+//  Copyright © 2020 Мас. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        scrollView.delegate = self
+    }
+
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateZoomFor(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+        scrollView.zoomScale = scale
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        updateZoomFor(size: view.bounds.size)
+    }
+}
+
